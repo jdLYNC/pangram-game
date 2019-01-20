0083fb2603b867a68485e67d8475346603b8480e
jdLYNC <joshlynch@live.co.uk>
2019-01-20 16:09:53 +0000

    created instance methods on Scoreboard class [tests passing]

--- Scoreboard.js
+++ Scoreboard.js
@@ -10,6 +10,30 @@ class Scoreboard extends Array{
   constructor(...entries) {
     super(...entries);
   }
+
+  /**
+   * Sorts and returns entries based on highest points.
+   * @param {number} n - Number of entries to return.
+   * @return {array} - Top scoring entries {Entry}.
+   */
+  getTopEntries(n) {
+    return this
+      .sort((entryA, entryB) => entryB.points - entryA.points)
+      .slice(0, n);
+  }
+
+  /**
+   * Generates a specific top scores array omitting the entry.word property for returning to the Angular app.
+   * @param {number} n - Number of entries to return.
+   * @return {array} - Array of top score objects.
+   */
+  getTopScores(n) {
+    return this
+      .getTopEntries(n)
+      .map(({ name, points }) => ({
+        name, points
+      }));
+  }
 }
 
 module.exports = Scoreboard;
